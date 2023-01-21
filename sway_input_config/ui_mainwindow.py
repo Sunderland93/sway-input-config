9eabdddc508b5b2e46dae946b2d9e6a6595ca2e2
Sunderland93 <samoilov.lex@gmail.com>
2023-01-21 15:21:34 +0400

    Fix first page

--- sway_input_config/ui_mainwindow.py
+++ sway_input_config/ui_mainwindow.py
@@ -549,7 +549,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
-        self.tabWidget.setCurrentIndex(2)
+        self.tabWidget.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
