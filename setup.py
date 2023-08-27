77ec9ea2832d319e474d419cdf9eb1b1f610c1e5
Sunderland93 <samoilov.lex@gmail.com>
2023-08-27 14:58:48 +0400

    Make it PIP friendly

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from importlib.metadata import entry_points
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='sway-input-config',
@@ -9,6 +13,8 @@ setup(
     author='Aleksey Samoilov',
     author_email='samoilov.lex@gmail.com',
     url='https://github.com/Sunderland93/sway-input-config',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     include_package_data=True,
     package_data={"": ["data/*", "ui/*", "langs/*"]},
@@ -17,7 +23,9 @@ setup(
         ('share/icons/hicolor/64x64/apps', ['sway-input-config.png']),
         ('share/metainfo', ['io.github.Sunderland93.sway-input-config.appdata.xml']),
     ],
-    install_requires=[],
+    install_requires=['PySide2',
+    'i3ipc',
+    ],
     entry_points={
         'gui_scripts': [
             'sway-input-config = sway_input_config.main:main'
