456933dcee32236634378ac1bad02f08818a4f3b
Sunderland93 <samoilov.lex@gmail.com>
2025-08-06 12:09:38 +0400

    Move SelectKeyboardLayout dialog to keyboard.py

--- sway_input_config/devices/keyboard.py
+++ sway_input_config/devices/keyboard.py
@@ -1,8 +1,8 @@
 import platform
 import re
-from PyQt6.QtWidgets import QTreeWidgetItem, QListView
+from PyQt6.QtWidgets import QTreeWidgetItem, QListView, QDialog, QListWidgetItem
 from PyQt6.QtCore import Qt
-from sway_input_config.dialogs import SelectKeyboardLayout
+from sway_input_config.ui_selectlayout import Ui_SelectKeyboardLayoutDialog
 from sway_input_config.utils import list_inputs_by_type, load_text_file
 
 os_name = platform.system()
@@ -139,7 +139,7 @@ class KeyboardSettings:
             self.settings["keyboard-use-settings"] = "false"
 
     def on_add_keyboard_layout(self):
-        self.dlg = SelectKeyboardLayout(self.layouts, self.variants)
+        self.dlg = SelectKeyboardLayout()
         if self.dlg.exec() == 1:
             lay_key = self.dlg.select_layout.layouts.currentItem().data(Qt.ItemDataRole.DisplayRole)
             lay_value = self.dlg.select_layout.layouts.currentItem().data(Qt.ItemDataRole.UserRole)
@@ -249,3 +249,62 @@ class KeyboardSettings:
         self.ui.repeatRate.setValue(self.ui.repeatRateSlider.value())
         self.ui.caps_lock.setChecked(False)
         self.ui.num_lock.setChecked(False)
+
+
+class SelectKeyboardLayout(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Add Keyboard Layout")
+        self.setModal(True)
+
+        self.select_layout = Ui_SelectKeyboardLayoutDialog()
+        self.select_layout.setupUi(self)
+
+        self.layout_code = ""
+        self.variant_code = ""
+
+        none_item = QListWidgetItem()
+        none_item.setData(Qt.ItemDataRole.UserRole, "")
+        none_item.setData(Qt.ItemDataRole.DisplayRole, "")
+        self.select_layout.variants.addItem(none_item)
+
+        for key, value in layouts:
+            item = QListWidgetItem(value)
+            item.setData(Qt.ItemDataRole.UserRole, key)
+            item.setData(Qt.ItemDataRole.DisplayRole, value)
+            self.select_layout.layouts.addItem(item)
+
+        self.layouts = layouts
+        self.variants = variants
+
+        self.select_layout.layouts.setCurrentItem(self.select_layout.layouts.item(0))
+        self.select_layout.variants.setCurrentItem(self.select_layout.variants.item(0))
+
+        self.select_layout.layouts.currentItemChanged.connect(self.on_layout_changed)
+        self.select_layout.buttonBox.rejected.connect(self.reject)
+        self.select_layout.buttonBox.accepted.connect(self.on_add_layout)
+
+    def on_layout_changed(self):
+        item = self.select_layout.layouts.currentItem()
+        self.select_layout.variants.clear()
+        none_item = QListWidgetItem()
+        none_item.setData(Qt.ItemDataRole.UserRole, "")
+        none_item.setData(Qt.ItemDataRole.DisplayRole, "")
+        self.select_layout.variants.addItem(none_item)
+        for key, values in self.variants:
+            value = values.split(":")[0]
+            description = values.split(":")[1]
+            if value in item.data(Qt.ItemDataRole.UserRole):
+                if "custom" not in item.data(Qt.ItemDataRole.UserRole):
+                    vitem = QListWidgetItem(description)
+                    vitem.setData(Qt.ItemDataRole.UserRole, key)
+                    vitem.setData(Qt.ItemDataRole.DisplayRole, description)
+                    self.select_layout.variants.addItem(vitem)
+        self.select_layout.variants.setCurrentItem(self.select_layout.variants.item(0))
+
+    def on_add_layout(self):
+        layout_item = self.select_layout.layouts.currentItem()
+        variant_item = self.select_layout.variants.currentItem()
+        self.layout_code = layout_item.data(Qt.ItemDataRole.UserRole)
+        self.variant_code = variant_item.data(Qt.ItemDataRole.UserRole) if variant_item else ""
+        self.accept()
--- sway_input_config/dialogs.py
+++ sway_input_config/dialogs.py
@@ -1,10 +1,9 @@
 import os
-from PyQt6.QtWidgets import QDialog, QListWidgetItem, QDialogButtonBox
+from PyQt6.QtWidgets import QDialog, QDialogButtonBox
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap
 from sway_input_config.ui_about import Ui_about
 from sway_input_config.ui_error_message import Ui_ErrorMessage
-from sway_input_config.ui_selectlayout import Ui_SelectKeyboardLayoutDialog
 
 class AboutDialog(QDialog):
     def __init__(self, dir_name, app_version):
@@ -34,62 +33,3 @@ class ErrorMessage(QDialog):
 
     def on_clicked_ok(self):
         self.close()
-
-
-class SelectKeyboardLayout(QDialog):
-    def __init__(self, layouts, variants, parent=None):
-        super().__init__(parent)
-        self.setWindowTitle("Add Keyboard Layout")
-        self.setModal(True)
-
-        self.select_layout = Ui_SelectKeyboardLayoutDialog()
-        self.select_layout.setupUi(self)
-
-        self.layout_code = ""
-        self.variant_code = ""
-
-        none_item = QListWidgetItem()
-        none_item.setData(Qt.ItemDataRole.UserRole, "")
-        none_item.setData(Qt.ItemDataRole.DisplayRole, "")
-        self.select_layout.variants.addItem(none_item)
-
-        for key, value in layouts:
-            item = QListWidgetItem(value)
-            item.setData(Qt.ItemDataRole.UserRole, key)
-            item.setData(Qt.ItemDataRole.DisplayRole, value)
-            self.select_layout.layouts.addItem(item)
-
-        self.layouts = layouts
-        self.variants = variants
-
-        self.select_layout.layouts.setCurrentItem(self.select_layout.layouts.item(0))
-        self.select_layout.variants.setCurrentItem(self.select_layout.variants.item(0))
-
-        self.select_layout.layouts.currentItemChanged.connect(self.on_layout_changed)
-        self.select_layout.buttonBox.rejected.connect(self.reject)
-        self.select_layout.buttonBox.accepted.connect(self.on_add_layout)
-
-    def on_layout_changed(self):
-        item = self.select_layout.layouts.currentItem()
-        self.select_layout.variants.clear()
-        none_item = QListWidgetItem()
-        none_item.setData(Qt.ItemDataRole.UserRole, "")
-        none_item.setData(Qt.ItemDataRole.DisplayRole, "")
-        self.select_layout.variants.addItem(none_item)
-        for key, values in self.variants:
-            value = values.split(":")[0]
-            description = values.split(":")[1]
-            if value in item.data(Qt.ItemDataRole.UserRole):
-                if "custom" not in item.data(Qt.ItemDataRole.UserRole):
-                    vitem = QListWidgetItem(description)
-                    vitem.setData(Qt.ItemDataRole.UserRole, key)
-                    vitem.setData(Qt.ItemDataRole.DisplayRole, description)
-                    self.select_layout.variants.addItem(vitem)
-        self.select_layout.variants.setCurrentItem(self.select_layout.variants.item(0))
-
-    def on_add_layout(self):
-        layout_item = self.select_layout.layouts.currentItem()
-        variant_item = self.select_layout.variants.currentItem()
-        self.layout_code = layout_item.data(Qt.ItemDataRole.UserRole)
-        self.variant_code = variant_item.data(Qt.ItemDataRole.UserRole) if variant_item else ""
-        self.accept()
